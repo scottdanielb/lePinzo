b1b91ab51810463a427050c73ff8a228af51e1bd
Daniel <danielmarroquin5224@gmail.com>
2022-06-22 20:40:28 -0500

    added logo

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/nosotros'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/productos'>
+          <Products />
+        </Route>
+        <Route
+          exact
+          path='/productos/:id'
+          children={<SingleProduct />}
+        />
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
